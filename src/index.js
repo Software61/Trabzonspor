7a69463d0e2b19d9bbcf518bc76788f89c2eba2d
Ömür BURUK <burukomur@gmail.com>
2017-12-03 02:02:00 +0300

    Login başlangıcı

--- src/index.js
+++ src/index.js
@@ -1,41 +1,52 @@
 import React, { Component } from 'react'
 
 import {
-	View,StyleSheet,Text
+	View,StyleSheet,Text,TextInput,Button
 } from 'react-native'
+const text="Merhaba";
 
 export default class IndexPage extends Component {
+	 constructor(props) {
+	    super(props);
+	    this.inputs = {
+	      userNameText: "Kullanıcı Adı",
+	      passwordText: 'Şifre'
+	    };
+	  }
+	
 	render(){
 		return (
-			 	<View style={styles.container}>
-				 	<View style={styles.otherContainer}>
-				 	</View>
-				 	<View style={styles.fixContainer}>
-				 	</View>
-				 	<View style={styles.fixContainer2}>
-				 	</View>
+			 	<View style={styles.container} >
+			 		<Text >{this.inputs.userNameText}</Text>
+				 	<TextInput
+				        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+				        value={this.text}
+				     />
+				 	<Text >{this.inputs.passwordText}</Text>
+				 	<TextInput
+				 	   ref= "username"
+				        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+				        value={this.text}
+				     />
+				     <Button
+				      onPress={this._onPress()}
+					  title="Giriş "
+					  color="#841584"
+					/>
+
 			 	</View>
+
 			 	
 			);
 	}
+	 _onPress() {
+  		alert(this.ref.username.value);
+ 	}
 }
 const styles = StyleSheet.create({
 	container : {
 		flex:1,
-		backgroundColor: 'pink'
-
-	},
-	otherContainer:{
-		flex:3,
-		backgroundColor: 'blue'
-	},
-	fixContainer:{
-		flex:8,
-		backgroundColor: 'yellow'
-	}
-	,
-	fixContainer2:{
-		flex:112,
-		backgroundColor: 'pink'
+		backgroundColor: 'pink',
+		margin:70
 	}
 });
